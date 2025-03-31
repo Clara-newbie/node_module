dbf393503dd74eaa77bd4b0720536435e17a2789
Clara-newbie <m.clara.massaro@gmail.com>
2025-03-31 15:58:03 +0200

    svolto esercizio 'Change HTML response'

---
+++ Esercizi_piattaforma/ChangeHTML.js
@@ -0,0 +1,20 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  // verifichiamo che la request arrivi
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  // così il server sa che la response dovrà essere costituita da un HTML
+  response.setHeader("Content-Type", "text/html");
+
+  response.end(
+    "<html><body><h1>This is the server response c:</h1></body></html>"
+  );
+});
+
+// il server accetta qui le requests del client
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
