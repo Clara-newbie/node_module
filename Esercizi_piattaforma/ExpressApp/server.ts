c94977c71dc949d2dadd761545b6abe4a84e5c12
Clara-newbie <m.clara.massaro@gmail.com>
2025-04-08 21:19:48 +0200

    svolto CRUD ex

--- Esercizi_piattaforma/ExpressApp/server.ts
+++ Esercizi_piattaforma/ExpressApp/server.ts
@@ -3,19 +3,58 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import "express-async-errors";
 
+// a che serve?
 dotenv.config();
 
 const app = express();
-const dbPort = process.env.DB_USER; // 3000
+const dbPort = process.env.DB_PORT; // 3000
 
-// è un metodo usato per configurare
-app.use(morgan("dev"));
+// USE è un metodo usato per configurare
+app.use(morgan("dev")); // Cosa fa MORGAN?
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.json(planets);
-});
 app.listen(dbPort, () => {
   console.log(`App listening on http://localhost${dbPort}`);
+});
+
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+});
+
+app.get("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const planetById = planets.find((p) => p.id === Number(id));
+
+  // per onestà intellettuale, avevo inserito solo la response
+  if (planetById) {
+    res.status(200).json(planetById);
+  } else {
+    res.status(404).json({ message: "Planet not found" });
+  }
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const myNewPlanet = { id, name };
+  planets = [...planets, myNewPlanet];
+
+  res.status(201).json({ msg: "Planet created successfully!" });
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  res.status(200).json({ msg: "Planet updated successfully!" });
+});
+
+app.delete("/api/planets/:id", (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter((p) => {
+    p.id !== Number(id);
+  });
+
+  res.status(200).json({ msg: "Planet deleted successfully!" });
 });
 
 // PIANETI
